6de7e2d5188170f2c39aca9a8437f48d84d31454
eugene isingizwe <eugeneisingizwe95@hotmail.com>
2022-09-22 21:09:07 -0600

    updated html/js

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//var parkApiKey = "pEl7mMX3orgycwe1sObUzotP8ZSa4vTgqOeL8Xf1"
+//https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=pEl7mMX3orgycwe1sObUzotP8ZSa4vTgqOeL8Xf1
+
+
+//Returns the date as a json to an array of object
+// function getApi() {
+//     // fetch request gets a list of all the repos for the node.js organization
+//     var requestUrl = '...';
+  
+//     fetch(requestUrl)//promise based function
+//       .then(function (response) {
+//         return response.json();
+//       })
+
+//fetchButton.addEventListener('click', getApi);
