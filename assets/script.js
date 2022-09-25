827ccb01a4d59db8e4b381a3ea0c7c8396c2c9b2
Madisen <madisenvogel@gmail.com>
2022-09-25 16:56:08 -0600

    searches printing to page as buttons

--- assets/script.js
+++ assets/script.js
@@ -3,19 +3,45 @@
 
 var weatherAPIKey = "117a7453ee844f288c7182432222509"
 
+var currentCity = "";
+
 // pulls 5 day forecast data from API
-function getWeatherForecast(city){
-    var weatherQueryURL = "http://api.weatherapi.com/v1/forecast.json?key=117a7453ee844f288c7182432222509&q=" + city + "&days=5&aqi=no&alerts=no";
+function getWeatherForecast(searchedCity){
+    var weatherQueryURL = "http://api.weatherapi.com/v1/forecast.json?key=117a7453ee844f288c7182432222509&q=" + searchedCity + "&days=5&aqi=no&alerts=no";
     fetch(weatherQueryURL)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log("data :" +JSON.stringify(data))
-        saveSearch(city);
-
-    })
+        saveSearch(searchedCity);
+        showHistory()
+    });
 }
+
+function showHistory(){
+    $('#search-history').empty();
+        if (localStorage.length===0){
+            if (previousSearch){
+                $('.city-search').attr("value", previousSearch);
+            } else {
+                $('.city-search').attr("value", "Colorado");
+        }
+    } else {
+        let prevCityKey= "cities" + (localStorage.length-1);
+        previousSearch=localStorage.getItem(prevCityKey);
+        $('.city-search').attr("value", previousSearch);
+        for (let i = 0; i < localStorage.length; i++) {
+            let city = localStorage.getItem("cities" + i);
+            let cityEl;
+                if (currentCity===""){
+                currentCity=previousSearch;
+                }
+            cityEl = `<button type="button">${city}</button>`;
+                $('#search-history').append(cityEl);
+        }
+}}
+
 
 var saveSearch = function(newSearch){
     let repeat = false;
@@ -31,6 +57,8 @@ var saveSearch = function(newSearch){
         localStorage.setItem('cities' + localStorage.length, newSearch);
     }
 }
+
+
 
 
 
