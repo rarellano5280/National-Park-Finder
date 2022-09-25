a5ff9370998f939a2acad1b96f78dbb9ce6d13c5
Madisen <madisenvogel@gmail.com>
2022-09-25 14:17:33 -0600

    connected weather API and added function to get 5 day forecast data with search button

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,31 @@
-//var parkApiKey = "pEl7mMX3orgycwe1sObUzotP8ZSa4vTgqOeL8Xf1"
+//var parkAPIKey = "pEl7mMX3orgycwe1sObUzotP8ZSa4vTgqOeL8Xf1"
 //https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=pEl7mMX3orgycwe1sObUzotP8ZSa4vTgqOeL8Xf1
 
+var weatherAPIKey = "117a7453ee844f288c7182432222509"
 
-//Returns the date as a json to an array of object
-// function getApi() {
-//     // fetch request gets a list of all the repos for the node.js organization
-//     var requestUrl = '...';
-  
-//     fetch(requestUrl)//promise based function
-//       .then(function (response) {
-//         return response.json();
-//       })
+// pulls 5 day forecast data from API
+function getWeatherForecast(city){
+    var weatherQueryURL = "http://api.weatherapi.com/v1/forecast.json?key=117a7453ee844f288c7182432222509&q=" + city + "&days=5&aqi=no&alerts=no";
+    fetch(weatherQueryURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log("data :" +JSON.stringify(data))
+    })
+}
 
-//fetchButton.addEventListener('click', getApi);
+// seach button event listener
+$("#submitBtn").on("click", function(event){
+    event.preventDefault();
+    var newCity = $("#city").val();
+    getWeatherForecast(newCity);
+});
+
+// makes search history buttons clickable
+$("#search-history").on("click", function(event){
+    event.preventDefault();
+    var historyBtn = event.target.textContent;
+    getCityWeather(historyBtn);
+    getForecast(historyBtn);
+});
