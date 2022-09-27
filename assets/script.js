d21143dc2117028f684d673b359cdda0d7c3da49
Duran11 <antonio11duran@gmail.com>
2022-09-26 18:44:01 -0600

    comments in js

--- assets/script.js
+++ assets/script.js
@@ -26,10 +26,12 @@ function getParkInfo(){
             console.log("Park Data :", data.data);
 })};
 
+// pulls map up
 $(document).ready(function () {
     $('#map').usmap({});
 });
 
+// lets you click on map to run function
 $('#map').usmap({
     click: function (event, data) {
         // Output the abbreviation of the state name to the console
