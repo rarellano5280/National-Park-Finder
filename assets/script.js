cb999fc862395579b7b248243a621b3e6b45d675
Madisen Vogel <madisen@Admins-Mac-mini.lan>
2022-09-28 15:07:11 -0600

    info formatting

--- assets/script.js
+++ assets/script.js
@@ -113,7 +113,7 @@ var toDoContainer = document.getElementById("thingstodo");
 
 function thingsToDo(data) {
     console.log(data);
-    var toDoTitle = document.createElement("h2");
+    var toDoTitle = document.createElement("h4");
     toDoTitle.textContent = "Featured Activities: ";
     var toDoList = document.createElement("ul");
     for (let i = 0; i < 4; i++) {
@@ -131,22 +131,23 @@ var parkdataContainer = document.getElementById("parkdata");
 
 function generalInfo(data) {
 
-    var infoBox = document.createElement("h2");
+    var infoBox = document.createElement("h1");
     infoBox.textContent = data[0].fullName;
     var img = document.createElement("img");
     img.setAttribute("src", data[0].images[0].url);
     infoBox.appendChild(img);
     parkdataContainer.appendChild(infoBox);
 
-    var infoTitle = document.createElement("h2");
+    var address = document.createElement("h4");
+    address.textContent = "Park Address: " + data[0].addresses[0].line1 + " " + data[0].addresses[0].city + ", " + data[0].addresses[0].stateCode + " " + data[0].addresses[0].postalCode;
+    infoBox.appendChild(address);
 
+    var infoTitle = document.createElement("h4");
 
 
     var infoList = document.createElement("ul");
     infoTitle.appendChild(infoList);
-    var address = document.createElement("li");
-    address.textContent = "Park Address: " + data[0].addresses[0].line1 + " " + data[0].addresses[0].city + ", " + data[0].addresses[0].stateCode + " " + data[0].addresses[0].postalCode;
-    infoList.appendChild(address);
+   
     
     var dayTitleArray = ["Monday: ", "Tuesday: ", "Wednesday: ", "Thursday: ", "Friday: ", "Saturday: " , "Sunday: "];
     
